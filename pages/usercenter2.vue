f39bc5036cc20d5c1c1ee8478238e24c00662d87
lvhaizhou <lvhaizhou2012@gmail.com>
2021-08-13 09:29:50 +0800

    拖拽上传

--- pages/usercenter2.vue
+++ pages/usercenter2.vue
@@ -1,12 +1,12 @@
 <!--
  * @Author: Lvhz
  * @Date: 2021-08-12 17:42:51
- * @Description: Description
+ * @Description: 版本2:拖拽上传
 -->
 <template>
   <div>
     <h1>用户中心</h1>
-    <div>
+    <div id="drag" ref="drag">
       <input type="file" name="file" @change="handleFilerChange">
     </div>
     <div>
@@ -27,8 +27,30 @@ export default {
   async mounted () {
     const res = await this.$http.get('/user/info')
     console.log(res)
+    this.bindEvents()
   },
   methods: {
+    bindEvents () {
+      const drag = this.$refs.drag
+      // 拖进来
+      drag.addEventListener('dragover', (e) => {
+        drag.style.borderColor = 'red'
+        e.preventDefault()
+      })
+      // 拖出去
+      drag.addEventListener('dragleave', (e) => {
+        drag.style.borderColor = '#eee'
+        e.preventDefault()
+      })
+      // 放下
+      drag.addEventListener('drop', (e) => {
+        const fileList = e.dataTransfer.files
+        drag.style.borderColor = '#eee'
+        this.file = fileList[0]
+
+        e.preventDefault()
+      })
+    },
     async uploadFiler () {
       // 因为文件是二进制的，所以要放到FormData之上
       const form = new FormData()
@@ -38,6 +60,7 @@ export default {
       console.log(res)
     },
     handleFilerChange (e) {
+      console.log('1111')
       const [file] = e.target.files
       if (!file) {
         return null
@@ -49,5 +72,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+#drag {
+  height: 100px;
+  line-height: 100px;
+  border: 2px dashed #eee;
+  text-align: center;
+  vertical-align: middle;
+}
 </style>
