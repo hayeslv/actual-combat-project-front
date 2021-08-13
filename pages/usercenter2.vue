3d3511cf3032efbd851014991d47c3a9135c4dc9
lvhaizhou <lvhaizhou2012@gmail.com>
2021-08-13 10:01:45 +0800

    进度条

--- pages/usercenter2.vue
+++ pages/usercenter2.vue
@@ -1,13 +1,16 @@
 <!--
  * @Author: Lvhz
  * @Date: 2021-08-12 17:42:51
- * @Description: 版本2:拖拽上传
+ * @Description: 版本2:拖拽上传 + 进度条
 -->
 <template>
   <div>
     <h1>用户中心</h1>
     <div id="drag" ref="drag">
       <input type="file" name="file" @change="handleFilerChange">
+    </div>
+    <div>
+      <el-progress :stroke-width="20" :text-inside="true" :percentage="uploadProgress" />
     </div>
     <div>
       <el-button @click="uploadFiler">
@@ -21,7 +24,8 @@
 export default {
   data () {
     return {
-      file: null
+      file: null,
+      uploadProgress: 0 // 进度
     }
   },
   async mounted () {
@@ -56,11 +60,14 @@ export default {
       const form = new FormData()
       form.append('name', 'file')
       form.append('file', this.file)
-      const res = await this.$http.post('/uploadfile', form)
+      const res = await this.$http.post('/uploadfile', form, {
+        onUploadProgress: (progress) => {
+          this.uploadProgress = Number(((progress.loaded / progress.total) * 100).toFixed(2))
+        }
+      })
       console.log(res)
     },
     handleFilerChange (e) {
-      console.log('1111')
       const [file] = e.target.files
       if (!file) {
         return null
